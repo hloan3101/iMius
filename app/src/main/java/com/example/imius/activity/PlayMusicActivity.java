b8a65f4a79d742c3179a63438a3f9a4f065ac346
hloan3101 <hloan975@gmail.com>
2023-02-16 21:54:54 +0700

    Connect Firebase

--- app/src/main/java/com/example/imius/activity/PlayMusicActivity.java
+++ app/src/main/java/com/example/imius/activity/PlayMusicActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.imius.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PlayMusicActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
     }
+
 }
