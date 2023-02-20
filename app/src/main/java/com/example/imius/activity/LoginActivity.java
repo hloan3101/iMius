57fad1f228dbc1097db98a480927891f14e17cc7
hloan3101 <hloan975@gmail.com>
2023-02-20 21:54:00 +0700

    Commit firebase

--- app/src/main/java/com/example/imius/activity/LoginActivity.java
+++ app/src/main/java/com/example/imius/activity/LoginActivity.java
@@ -2,15 +2,43 @@ package com.example.imius.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.imius.R;
+import com.example.imius.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    private void initListener (){
+        binding.activityLoginBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.activityLoginTvForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgetPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.activityLoginTvNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/imius/activity/SignUpActivity.java
+++ app/src/main/java/com/example/imius/activity/SignUpActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.imius.R;
+import com.example.imius.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
