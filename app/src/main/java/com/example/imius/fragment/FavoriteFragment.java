8a36138133d5a11b60900a21a80dfce45383184b
mkhanh01 <minhkhanht1905@gmail.com>
2023-04-26 01:20:39 +0700

    AndroidManifest.xml

--- app/src/main/java/com/example/imius/fragment/FavoriteFragment.java
+++ app/src/main/java/com/example/imius/fragment/FavoriteFragment.java
@@ -20,6 +20,7 @@ public class FavoriteFragment extends Fragment {
         return fragment;
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
