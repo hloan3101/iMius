0d95e6a421d34796d7dc7cfd916ab9dd39c78e2f
hloan3101 <hloan975@gmail.com>
2023-05-04 23:05:35 +0700

    add image

--- app/src/main/java/com/example/imius/adapter/LibraryPlaylistAdapter.java
+++ app/src/main/java/com/example/imius/adapter/LibraryPlaylistAdapter.java
@@ -97,6 +97,7 @@ public class LibraryPlaylistAdapter extends RecyclerView.Adapter<LibraryPlaylist
         Bundle bundle =  new Bundle();
         bundle.putInt("idLibraryPlaylist", libraryPlaylist.getIdLibraryPlaylist());
         bundle.putString("nameLibraryPlaylist", libraryPlaylist.getNameLibraryPlaylist());
+
         bundle.putString("imgPlaylistLibrary", image);
 
         intent.putExtras(bundle);
